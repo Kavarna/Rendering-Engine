1d041419eb32b7d73c919e1ac5a3c8dfba427254
Stefan Dobrica <sdobrica188@gmail.com>
2022-07-10 21:38:45 +0200

    Added some tests for ThreadPool and fixed some minor bugs

--- Renderer/tests/Threading.cpp
+++ Renderer/tests/Threading.cpp
@@ -50,5 +50,183 @@ namespace
 
     }
 
-    INSTANTIATE_TEST_CASE_P(ThreadingTests, Threading, testing::Range(0, 100));
+    TEST_P(Threading, ThreadingImmediateCompletenessPolicyExitASAP)
+    {
+        EXPECT_NO_THROW(auto threadPool = ThreadPool::Get());
+
+        auto threadPool = ThreadPool::Get();
+        EXPECT_NE(threadPool, nullptr);
+
+        const unsigned int numbersCount = 1000;
+        std::mutex numbersMutex;
+        std::set<uint32_t> numbers;
+
+        std::shared_ptr<struct Task> first_task;
+
+        for (uint32_t j = 0; j < numbersCount; ++j)
+        {
+            auto task = threadPool->ExecuteDeffered([j, &numbersMutex, &numbers]()
+            {
+                std::unique_lock<std::mutex> lock(numbersMutex);
+                numbers.insert(j);
+                std::this_thread::sleep_for(std::chrono::nanoseconds(j * 10));
+            });
+            if (j == 0)
+            {
+                first_task = task;
+            }
+        }
+
+        threadPool->Wait(first_task, ThreadPool::WaitPolicy::EXIT_ASAP);
+
+        EXPECT_NE(numbers.find(0), numbers.end());
+        threadPool->CancelRemainingTasks();
+        threadPool->WaitForAll();
+    }
+
+    TEST_P(Threading, ThreadingImmediateCompletenessPolicySearchAndExecute)
+    {
+        EXPECT_NO_THROW(auto threadPool = ThreadPool::Get());
+
+        auto threadPool = ThreadPool::Get();
+        EXPECT_NE(threadPool, nullptr);
+
+        const unsigned int numbersCount = 1000;
+        std::mutex numbersMutex;
+        std::set<uint32_t> numbers;
+
+        std::shared_ptr<struct Task> first_task;
+
+        for (uint32_t j = 0; j < numbersCount; ++j)
+        {
+            auto task = threadPool->ExecuteDeffered([j, &numbersMutex, &numbers]()
+            {
+                std::unique_lock<std::mutex> lock(numbersMutex);
+                numbers.insert(j);
+                std::this_thread::sleep_for(std::chrono::nanoseconds(j * 10));
+            });
+            if (j == 0)
+            {
+                first_task = task;
+            }
+        }
+
+        threadPool->Wait(first_task, ThreadPool::WaitPolicy::SEARCH_AND_EXECUTE_THEN_EXIT);
+
+        EXPECT_NE(numbers.find(0), numbers.end());
+        threadPool->CancelRemainingTasks();
+        threadPool->WaitForAll();
+    }
+
+    TEST_P(Threading, ThreadingImmediateCompletenessPolicyExecuteThenExit)
+    {
+        EXPECT_NO_THROW(auto threadPool = ThreadPool::Get());
+
+        auto threadPool = ThreadPool::Get();
+        EXPECT_NE(threadPool, nullptr);
+
+        const unsigned int numbersCount = 1000;
+        std::mutex numbersMutex;
+        std::set<uint32_t> numbers;
+
+        std::shared_ptr<struct Task> first_task;
+
+        for (uint32_t j = 0; j < numbersCount; ++j)
+        {
+            auto task = threadPool->ExecuteDeffered([j, &numbersMutex, &numbers]()
+            {
+                std::unique_lock<std::mutex> lock(numbersMutex);
+                numbers.insert(j);
+                std::this_thread::sleep_for(std::chrono::nanoseconds(j * 10));
+            });
+            if (j == 0)
+            {
+                first_task = task;
+            }
+        }
+
+        threadPool->Wait(first_task, ThreadPool::WaitPolicy::EXECUTE_THEN_EXIT);
+
+        EXPECT_NE(numbers.find(0), numbers.end());
+        threadPool->CancelRemainingTasks();
+        threadPool->WaitForAll();
+    }
+
+    TEST_P(Threading, ThreadingCompletenessWithOneElementPolicySearchAndExecute)
+    {
+        EXPECT_NO_THROW(auto threadPool = ThreadPool::Get());
+
+        auto threadPool = ThreadPool::Get();
+        EXPECT_NE(threadPool, nullptr);
+
+        const unsigned int numbersCount = 1000;
+        std::mutex numbersMutex;
+        std::set<uint32_t> numbers;
+
+        std::shared_ptr<struct Task> task =
+            threadPool->ExecuteDeffered([&numbersMutex, &numbers]()
+        {
+            std::unique_lock<std::mutex> lock(numbersMutex);
+            numbers.insert(0);
+            std::this_thread::sleep_for(std::chrono::nanoseconds(100));
+        });
+
+        threadPool->Wait(task, ThreadPool::WaitPolicy::SEARCH_AND_EXECUTE_THEN_EXIT);
+
+        EXPECT_NE(numbers.find(0), numbers.end());
+        threadPool->WaitForAll();
+    }
+
+
+    TEST_P(Threading, ThreadingCompletenessWithOneElementPolicyExitASAP)
+    {
+        EXPECT_NO_THROW(auto threadPool = ThreadPool::Get());
+
+        auto threadPool = ThreadPool::Get();
+        EXPECT_NE(threadPool, nullptr);
+
+        const unsigned int numbersCount = 1000;
+        std::mutex numbersMutex;
+        std::set<uint32_t> numbers;
+
+        std::shared_ptr<struct Task> task =
+            threadPool->ExecuteDeffered([&numbersMutex, &numbers]()
+        {
+            std::unique_lock<std::mutex> lock(numbersMutex);
+            numbers.insert(0);
+            std::this_thread::sleep_for(std::chrono::nanoseconds(100));
+        });
+
+        threadPool->Wait(task, ThreadPool::WaitPolicy::EXIT_ASAP);
+
+        EXPECT_NE(numbers.find(0), numbers.end());
+        threadPool->WaitForAll();
+    }
+
+    TEST_P(Threading, DISABLED_ThreadingCompletenessWithOneElementPolicyExecuteThenExit)
+    {
+        EXPECT_NO_THROW(auto threadPool = ThreadPool::Get());
+
+        auto threadPool = ThreadPool::Get();
+        EXPECT_NE(threadPool, nullptr);
+
+        const unsigned int numbersCount = 1000;
+        std::mutex numbersMutex;
+        std::set<uint32_t> numbers;
+
+        std::shared_ptr<struct Task> task =
+            threadPool->ExecuteDeffered([&numbersMutex, &numbers]()
+        {
+            std::unique_lock<std::mutex> lock(numbersMutex);
+            numbers.insert(0);
+            std::this_thread::sleep_for(std::chrono::nanoseconds(100));
+        });
+
+        threadPool->Wait(task, ThreadPool::WaitPolicy::EXECUTE_THEN_EXIT);
+
+        EXPECT_NE(numbers.find(0), numbers.end());
+        threadPool->WaitForAll();
+    }
+
+    INSTANTIATE_TEST_SUITE_P(ThreadingTests, Threading, testing::Range(0, 100));
 }
